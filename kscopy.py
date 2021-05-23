edc88a7e2a0947d948889a15984c2e2e1ade848b
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2021-05-22 17:10:39 -0700

    temp

--- kscopy.py
+++ kscopy.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
-from util import make_bounds
+import logging
+import time
+from util import make_bounds, TextManager
+import threading
 
 
 class MirrorAssembly(object):
@@ -47,15 +50,132 @@ class IsosceleseMirrorAssembly(MirrorAssembly):
         super(IsosceleseMirrorAssembly, self).__init__(corners=corners)
 
 
-class Kalleidoscope(object):
-    def __init__(self, resolution):
+class Kaleidoscope(object):
+
+    def __init__(self,
+                 mirrors=None,
+                 resolution=(1024, 1024),
+                 fov_deg=45.0,
+                 eye_scope_cm=4.0,
+                 scope_image_scm=20.0):
+        """
+        Kaleidoscope object, main GUI.
+        :param mirrors: MirrorAssmebly object, defaults to Isosceles triangle if None
+        :param resolution:  h, w (output)
+        :param fov_deg:  field of view
+        :param eye_scope_cm:  distance, float
+        :param scope_image_scm:  distance, float
+        """
+        # params
+        theta_deg = 15.0
         self._resolution = resolution
-        self._mirrors = IsosceleseMirrorAssembly(theta_deg=15.0, h_cm=5.0)
+        self._mirrors = IsosceleseMirrorAssembly(theta_deg=theta_deg, h_cm=5.0) if mirrors is None else mirrors
+        self._dims = {'eye_scope_cm': eye_scope_cm,
+                      'fov': np.deg2rad(fov_deg),
+                      'scope_img_cm': scope_image_scm,
+                      'theta_deg': theta_deg}
+        self._eye_scope_cm = eye_scope_cm
+
+        # state
+        self._running = False
+        self._finish = False
+        i, j = np.arange(resolution[0]), np.arange(resolution[1])
+        self._all_coords = np.dstack(np.meshgrid(i, j)).reshape(-1, 2)
+        self._cur_img = None
+        self._ray_map = None
         self._image = None
+        self._frame_in = None
+        self._fps_out = 1.0 / 0.010
         self._image_bounds = None
-        self._eye_scope_cm = 4.0
-        self._eye_image_plane_cm = 2.0
-        self._scope_img_cm = 20.0
+        self._dpi = None
+        self._live = None
+        self._cam = None
+
+        self._cam_ind = None
+        self._cam_thread = None
+        self._n_out_frames = None
+        self._n_in_frames = None
+        self._start_t = None
+
+        # GUI
+        self._hotkeys = self._make_hotkeys()
+        self._text = TextManager()
+        self._out_window_name = "Kscopy"
+        cv2.namedWindow(self._out_window_name, cv2.WINDOW_NORMAL)
+        self._mouse_state = {"l_button": None,
+                             "r_button": None,
+                             "pos": None,
+                             "held_pos": None,
+                             "abs_coords": None}
+        cv2.setMouseCallback(self._out_window_name, self._mouse_event)
+
+    def _image_proc(self):
+        while not self._finish:
+            if self._cur_img is None:
+                logging.warning("No image to display.")
+                time.sleep(.25)
+
+            image = self._annotate(self._cur_img)
+            cv2.imshow(self._out_window_name, image)
+            k = cv2.waitKey(int(1.0 / self._fps_out))  # time this better...
+            self._do_keyboard(k)
+
+    def quit(self):
+        self.stop()
+        self.shutdown()
+
+    def _adjust(self, param, dir):
+        if param not in self._dims:
+            raise Exception("parameter not found in dims:  %s" % (param,))
+        self._dims[param] += dir
+        self._generate_ray_map()
+
+    def _make_hotkeys(self):
+        def _toggle_state():
+            if self._running:
+                self.stop()
+            else:
+                self.start()
+
+        return {'q': {'name': 'Quit',
+                      'dispatch': self.quit},
+                ' ': {'alt': '[SPACE]',
+                      'name': 'Start / Stop',
+                      'dispatch': _toggle_state},
+                'a': {'name': "Increase eye-scope distance.",
+                      'dispatch': self._adjust,
+                      'params': ("eye_scope_cm", 1)},
+                'z': {'name': "Decrease eye-scope distance.",
+                      'dispatch': self._adjust,
+                      'params': ("eye_scope_cm", -1)},
+                's': {'name': "Increase scope-image distance.",
+                      'dispatch': self._adjust,
+                      'params': ("scope_image_cm", 1)},
+                'x': {'name': "Decrease scope-image distance.",
+                      'dispatch': self._adjust,
+                      'params': ("scope_image_cm", -1)},
+                }
+
+    def _do_keyboard(self, k):
+        for hotkey in self._hotkeys:
+            if k & 0xff == ord(hotkey):
+                params = {}
+                if 'params' in self._hotkeys[hotkey]:
+                    params.update(self._hotkeys[hotkey]['params'])
+                self._hotkeys[hotkey]['dispatch'](**params)
+
+    def _mouse_event(self, event, x, y, flags, param):
+        pass
+
+    def _annotate(self, img):
+        fps = self._n_out_frames / (time.time() - self._start_t)
+        fps_out = "FPS out:  %.3f" % (fps,)
+        if self._image is None:
+            fps_in = self._n_in_frames / (time.time() - self._start_t)
+            fps_out = "FPS in:  %.3f -- %s" % (fps_in, fps_out)
+        self._text.add_text(fps_out, pos=(self._resolution[0] - 30, 30), age=-1)
+        logging.info(fps_out)
+        return self._text.render(img)
 
     def _set_img_bounds(self, dpi):
         width = self._image.shape[1] / dpi
@@ -63,15 +183,62 @@ class Kalleidoscope(object):
         self._image_bounds = make_bounds([[-width / 2.0, -height / 2.0, ],
                                           [width / 2.0, height / 2.0, ], ])
 
-    def show(self, img, dpi=100.0):
-        self._image = img
+    def stop(self):
+        logging.info("Stopping!")
+        self._finish = True
+        if self._cam_thread is not None:
+            self._cam_thread.join()
+
+    def _setup(self):
+        self._n_out_frames = 0
+        self._start_t = time.time()
+        self._finish = False
+        self._started = True
+        self._update_ray_map()
+
+    def view_image(self, image, dpi=100.0):
+        logging.info("Starting with image:  %s (%s dpi)" % (self._image.shape, self._dpi))
+        self._image = image
+        self._dpi = dpi
         self._set_img_bounds(dpi)
-        self._make_ray_mask()
+        self._setup()
+        self._image_proc()
+        logging.info("Image view Stopped.")
 
-    def _make_ray_mask(self):
+    def view_live(self, cam_ind):
+        self._cam_ind = cam_ind
+        logging.info("Starting live!")
+        self._setup()
+        self._cam_thread = threading.Thread(target=self._cam_proc)
+        self._n_in_frames = None
+        self._cam_thread.start()
+        logging.info("Live view started...")
 
+    def _cam_proc(self):
+        self._cam = cv2.VideoCapture(self._cam_ind)
+        while not self._finish:
+            r, frame = self._cam.read()
+            if not r:
+                logging.warning("No camera data, waiting for 1 sec...")
+                time.sleep(1.0)
+                continue
+            self._n_in_frames += 1
+            self._frame_in = frame
+            self._update_out_image()
+
+        logging.info("Live view stopped.")
+
+    def _update_out_image(self):
+        return np.zeros(shape=self._resolution)
+
+    def _update_ray_map(self):
         # calculate dimensions of image plane & it's bounds of the rays.
         origin = np.ones(3).reshape(1, 3)
+
+        # define image plane
+        i_plane_z = np.arctan2(self._dims['fov'] / 2.0) * self._dims['d']
+        asgaerga
+
         scope_extent = self._mirrors.get_bounds()
         height_px_per_cm = self._resolution[0] / (scope_extent['top'] - scope_extent['bottom'])
         width_px_per_cm = self._resolution[1] / (scope_extent['right'] - scope_extent['left'])
@@ -87,12 +254,10 @@ class Kalleidoscope(object):
             else:
                 scale = -scope_extent['right'] / self._eye_scope_cm
 
-                
         image_plane_scope = make_bounds([[]])
 
 
-
 if __name__ == "__main__":
-    scope = Kalleidoscope()
+    scope = Kaleidoscope()
     img = cv2.imread('test_img.jpg')
-    scope.show(img)
+    scope.start_image(img)
--- util.py
+++ util.py
@@ -1,4 +1,124 @@
+import time
+import os
 import numpy as np
+import cv2
+
+
+class TextManager(object):
+
+    """
+    For adding text to output streams.
+    "Set it and forget it" -- add text with expiration times, etc.
+    """
+
+    def __init__(self):
+        self._items = []
+
+    def add_text(self, text, pos, age=0, font=cv2.FONT_HERSHEY_SIMPLEX, font_scale=1, color=(255, 255, 255),
+                 thickness=1, linestyle=cv2.LINE_AA):
+        """
+        Most args are for cv2 puttext.
+        :param text: string
+        :param pos: (row, col)
+        :param age: 0 for indefinite, or seconds, or -1 for just once
+        :param font: cv2 param
+        :param font_scale: cv2 param
+        :param color: cv2 param
+        :param thickness: cv2 param
+        :param linestyle: cv2 param
+        :return: index, used to delete before expiration
+        """
+        t = {'text': text,
+             'pos': pos,
+             'fontScale': font_scale,
+             'font': font,
+             'color': color,
+             'thickness': thickness,
+             'linestyle': linestyle,
+             'age': age,
+             'start': time.time()
+             }
+        (w, h), _ = cv2.getTextSize(t['text'], t['font'], t['fontScale'], t['thickness'])
+        self._items.append(t)
+        return w, h
+
+    def add_lines(self, lines, pos, leading=0.4, *args, **kwargs):
+        """
+        Put multiple lines.
+        :param lines:  list of text lines
+        :param pos: lower left corner of first line of text
+        :param leading: spacing of lines
+        :param args:  additional args to add_text
+        :param kwargs: additional args to add_text
+        :return:  lower left corner of last line of text
+        """
+        print("Adding lines")
+        px = pos[0]
+        py = pos[1]
+        for line in lines:
+            _, h = self.add_text(line, (px, py), *args, **kwargs)
+            py += int(float(h) * (1.0 + leading))
+        return py
+
+    def remove(self, item):
+        self._items.pop(item)
+
+    def _check_ages(self):
+        valid = [i for i in self._items if i['age'] <= 0 or time.time() - i['start'] < i['age']]
+        if len(valid) < len(self._items):
+            print("Aged out!")
+        self._items = valid
+
+    def render(self, img):
+
+        self._check_ages()
+
+        image = img.copy()
+        for t in self._items:
+            image = cv2.putText(image, t['text'], t['pos'], t['font'], t['fontScale'], t['color'], t['thickness'],
+                                t['linestyle'])
+        singles_removed = [i for i in self._items if i['age']>=0]
+        self._items = singles_removed
+        return image
+
+
+def get_index_grid(img_shape, grid_shape):
+    r = np.linspace(0, img_shape[0] - 1, grid_shape[0] + 1).astype(np.int64)
+    c = np.linspace(0, img_shape[1] - 1, grid_shape[1] + 1).astype(np.int64)
+    return r, c
+
+
+def make_int_grid(shape):
+    return np.zeros(np.prod(shape), dtype=np.int64).reshape(shape)
+
+
+def check_make_dir(path, uniquify=False):
+    if os.path.exists(path):
+        if uniquify:
+            index = 0
+            p_temp = "%s_%i" % (path, index)
+            while os.path.exists(p_temp):
+                index += 1
+                p_temp = "%s_%i" % (path, index)
+            path = p_temp
+    else:
+        os.mkdir(path)
+    return path
+
+
+def test_text_manager():
+    tm = TextManager()
+    img = np.zeros(500 * 350 * 3, dtype=np.uint8).reshape((350, 500, 3))
+    tm.add_text("blah", (100, 30))
+    tm.add_lines(['This', 'is', "a test."], (300, 60))
+    """
+    import matplotlib.pylab as plt
+    plt.imshow(tm.render(img))
+    plt.show()
+    """
+
+if __name__ == "__main__":
+    test_text_manager()
 
 
 def make_bounds(coords):
